766e49e5daf58bb00b762fdb7629495d642bad5c
Noah Elzner <78953604+enteraga6@users.noreply.github.com>
2023-06-01 16:37:49 -0700

    refactor file structure

--- internal/bazel/build.sh
+++ internal/bazel/build.sh
@@ -1,1 +1,18 @@
+set -euo pipefail
 
+mkdir binaries
+
+bazel build ${{ inputs.flags }} ${{ inputs.targets }}
+
+echo "built correctly"
+
+IFS=' ' read -r -a targets <<< "${{ inputs.targets }}"
+for TARGET in "${targets[@]}"; do
+  echo "start loop"
+  CD_PATH=${TARGET%:*}
+  echo "took off colon"
+  CD_PATH=${CD_PATH////}
+  echo "took off slanted"
+  BINARY_NAME=${TARGET#*:}
+  cp "bazel-bin/$CD_PATH/$BINARY_NAME" ./binaries
+done
